82f0f084476c46d43fc27cb33e87b97822b64928
Steve Rayner <srayner02@gmail.com>
2024-06-26 22:45:01 +0100

    built out side nav

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,17 +1,14 @@
-import Navbar from "../ui/dashboard/navbar";
-import Sidebar from "../ui/dashboard/sidebar";
+import Navbar from "../ui/dashboard/navbar/navbar";
+import Sidebar from "../ui/dashboard/sidebar/sidebar";
 
 const Dashboard = () => {
-
-    return (
-        <main>
-            <h1>Dashboard</h1>
-            <Navbar />
-            <Sidebar />
-        </main>
-        
-    );
-
-}
+  return (
+    <main>
+      <h1>Dashboard</h1>
+      <Navbar />
+      <Sidebar />
+    </main>
+  );
+};
 
 export default Dashboard;
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
-import "./globals.css";
+import { Inter } from "next/font/google";
+import "./ui/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Admin",
@@ -8,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
